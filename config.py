98d77acb68b0de8871587f7a7b70146fe8ca2b68
lukerspace.hsieh <lukerspace@gmail.com>
2021-06-12 18:07:36 +0800

    watch-list

---
+++ config.py
@@ -0,0 +1,8 @@
+api_key = 'AHJLYZDA2MSWONNFAVVL837QNXVKUSMP@AMER.OAUTHAP'
+token_path = 'token'
+redirect_uri = 'https://localhost'
+
+account_id = 123456789
+
+# put path to chromedriver here
+chromedriver_path='/Users/STEVEN/Desktop/software/python/tdameritrade-master/chromedriver'
--- trade.py
+++ trade.py
@@ -12,46 +12,44 @@ except FileNotFoundError:
     with webdriver.Chrome(executable_path=config.chromedriver_path) as driver:
         c = auth.client_from_login_flow(
             driver, config.api_key, config.redirect_uri, config.token_path)
-
-# get price history for a symbol
-# r = c.get_price_history('AAPL',
-#         period_type=client.Client.PriceHistory.PeriodType.YEAR,
-#         period=client.Client.PriceHistory.Period.TWENTY_YEARS,
-#         frequency_type=client.Client.PriceHistory.FrequencyType.DAILY,
-#         frequency=client.Client.PriceHistory.Frequency.DAILY)
-
-# print(json.dumps(r.json(), indent=4))
-
-# get a stock quote
-response = c.get_quote('AAPL')
-# print(response.json())
-print("1.取得股票報價")
-print(json.dumps(response.json(),indent=4))
-
-
-# # get stock fundamental data
-response = c.search_instruments(['AAPL', 'BA'], c.Instrument.Projection.FUNDAMENTAL)
-print("2.取得股票資料")
+print(json.dumps(c.get_quote("AAPL").json(),indent=4))
+response = c.search_instruments("AAPL", c.Instrument.Projection.FUNDAMENTAL)
 print(json.dumps(response.json(), indent=4))
+
+watchlist=["AMZN","AAPL","TSLA","MSFT","BA","AMD"]
+fundamental= c.search_instruments(watchlist, c.Instrument.Projection.FUNDAMENTAL)
+for name in watchlist:
+    market=c.get_quote(name)
+    marketInfo=market.json()
+    print("--------------")
+    print(marketInfo[name]["symbol"],":",marketInfo[name]["mark"])
+    print("取得52周high:",
+        # response.json()[name]["fundamental"]["symbol"],
+        fundamental.json()[name]["fundamental"]["high52"]
+    )
+    print("取得peRatio",
+        # response.json()[name]["fundamental"]["symbol"],
+        fundamental.json()[name]["fundamental"]["peRatio"]
+    )
+    print("距離前高ratio:",marketInfo[name]["mark"]/fundamental.json()[name]["fundamental"]["high52"] )
 
 
 # # get option chain
-response = c.get_option_chain('AAPL')
-print("3.取得選擇權資料")
-print(json.dumps(response.json(), indent=4))
+# response = c.get_option_chain('AAPL')
+# print("3.取得選擇權資料")
+# print(json.dumps(response.json(), indent=4))
 
 
 # # get all call options
-response = c.get_option_chain('AAPL', contract_type=c.Options.ContractType.CALL)
-print("4.取得所有選擇權資料")
+# response = c.get_option_chain('AAPL', contract_type=c.Options.ContractType.CALL)
+# print("4.取得所有選擇權資料")
 # print(json.dumps(response.json(), indent=4))
 
 
 # # get call options for a specific strike
-response = c.get_option_chain('AAPL', contract_type=c.Options.ContractType.CALL, strike=300)
-print("4.取得特定執行價格選擇權資料")
-print(json.dumps(response.json(), indent=4))
-
+# response = c.get_option_chain('AAPL', contract_type=c.Options.ContractType.CALL, strike=130t)
+# print("4.取得特定執行價格選擇權資料")
+# print(json.dumps(response.json(), indent=4))
 
 # # get call options for a specific strike and date range
 # start_date = datetime.datetime.strptime('2020-04-24', '%Y-%m-%d').date()
@@ -74,3 +72,12 @@ print(json.dumps(response.json(), indent=4))
 # response = c.place_order(config.account_id, builder.build())
 
 # print(response)
+
+# get price history for a symbol
+# r = c.get_price_history('AAPL',
+#         period_type=client.Client.PriceHistory.PeriodType.YEAR,
+#         period=client.Client.PriceHistory.Period.TWENTY_YEARS,
+#         frequency_type=client.Client.PriceHistory.FrequencyType.DAILY,
+#         frequency=client.Client.PriceHistory.Frequency.DAILY)
+# print(json.dumps(r.json(), indent=4))
+
